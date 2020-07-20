d3e5cdc56682f876a7c73d9125065fed14668e59
skeown13 <code@keown.net>
2020-07-20 13:43:11 -0700

    routing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs")
 const http = require("http")
+const url = require("url")
 
 //////////////////////////////////////////////////
 // FILES
@@ -31,6 +32,20 @@ const http = require("http")
 
 //SERVER
 const server = http.createServer((req, res) => {
+  const pathName = req.url
+
+  if(pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW")
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT")
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world"
+    })
+    res.end("<h1>Page not found!</h1>")
+  }
+
   res.end("Hello from the server!")
 })
 
