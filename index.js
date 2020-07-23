9ef93dd0624a5f53e1c1d0fbb97c2038edf3aff1
skeown13 <code@keown.net>
2020-07-23 13:56:04 -0700

    updated the Server for the Overview Page. We map over the data in the json file. then replace our placeholders with the information from the json file.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs")
 const http = require("http")
 const url = require("url")
+const { profileEnd } = require("console")
 
 //////////////////////////////////////////////////
 // FILES
@@ -31,19 +32,52 @@ const url = require("url")
 //////////////////////////////////////////////////////////
 
 //SERVER
+// Save the read at the beginning so that it is only read once and then called upon when needed. There is no need to read everytime that it is called. Can do the synchronis version because it is in top level code and only runs once upon first load.
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+  output = output.replace(/{%IMAGE%}/g, product.image)
+  output = output.replace(/{%FROM%}/g, product.from)
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+  output = output.replace(/{%QUANTITY%}/g, product.quantity)
+  output = output.replace(/{%PRICE%}/g, product.price)
+  output = output.replace(/{%DESCRIPTION%}/g, product.description)
+  output = output.replace(/{%ID%}/g, product.id)
+
+  if(!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+  }
+  return output
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, "utf-8")
+const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, "utf-8")
+const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, "utf-8")
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
 const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
   const pathName = req.url
 
+  //Overview Page
   if(pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW")
+    res.writeHead(200, { "Content-type": "text/html" })
+
+    // arrow function will implicitly return as there is no curly braces
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join("")
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml)
+
+    res.end(output)
+
+  // Product Page
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT")
+
+  // API
   } else if (pathName === "/api") {
     res.writeHead(200, { "Content-type": "application/json" })
     res.end(data)
+
+  // Not Found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
