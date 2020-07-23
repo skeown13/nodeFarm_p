40823f8b36d74503f427e82b94b121a7eb127b81
skeown13 <code@keown.net>
2020-07-23 14:21:58 -0700

    destructured url.parse(req.url, true) to obtain the query and pathname. used the query.id as the location from dataObj. again called replaceTemplate() to create each page

--- index.js
+++ index.js
@@ -56,10 +56,11 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
 const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url
+
+  const { query, pathname } = url.parse(req.url, true)
 
   //Overview Page
-  if(pathName === "/" || pathName === "/overview") {
+  if(pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" })
 
     // arrow function will implicitly return as there is no curly braces
@@ -69,11 +70,14 @@ const server = http.createServer((req, res) => {
     res.end(output)
 
   // Product Page
-  } else if (pathName === "/product") {
-    res.end("This is the PRODUCT")
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" })
+    const product = dataObj[query.id]
+    const output = replaceTemplate(tempProduct, product)
+    res.end(output)
 
   // API
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" })
     res.end(data)
 
